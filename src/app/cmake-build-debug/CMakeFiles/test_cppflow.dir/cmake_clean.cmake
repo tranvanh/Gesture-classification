72a0addf68cd21b044c73478b97816d638b4799f
tranvanh <tranvietanhcz@gmail.com>
2021-02-26 14:38:45 +0100

    init project

---
+++ src/app/cmake-build-debug/CMakeFiles/test_cppflow.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_cppflow.dir/main.cpp.o"
+  "test_cppflow"
+  "test_cppflow.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_cppflow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/app/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "include/cppflow/ops.h"
+#include "include/cppflow/model.h"
+
+
+int main() {
+
+    auto input = cppflow::fill({10, 5}, 1.0f);
+    cppflow::model model("./model");
+    auto output = model(input);
+
+    std::cout << output << std::endl;
+
+    auto values = output.get_data<float>();
+
+    for (auto v : values) {
+        std::cout << v << std::endl;
+    }
+    return 0;
+}
