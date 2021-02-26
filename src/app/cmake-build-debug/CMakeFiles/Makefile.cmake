5e3dae93562a415dbe8145bffacf8717729b8b75
tranvanh <tranvietanhcz@gmail.com>
2021-02-26 16:33:21 +0100

    created test tensor

--- src/app/cmake-build-debug/CMakeFiles/Makefile.cmake
+++ src/app/cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -48,5 +48,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/test_cppflow.dir/DependInfo.cmake"
+  "CMakeFiles/app_gestures.dir/DependInfo.cmake"
   )
