572f0e61e42ebcf91b3a2965effea5900d96a00f
tranvanh <tranvietanhcz@gmail.com>
2021-03-01 15:15:20 +0100

    MinMax data normalization

--- src/app/model/CModelWrapper.cpp
+++ src/app/model/CModelWrapper.cpp
@@ -3,8 +3,19 @@
 //
 
 #include "CModelWrapper.h"
-int CModelWrapper::preditct(const std::vector<float> input_data){
-    auto output = this->model(cppflow::tensor(input_data,{1,200,31}));
+
+void CModelWrapper::min_max_scaler(std::vector<float>& output, const std::vector<std::vector<float>> input)
+{
+    int ix = 0;
+    for (int i = 0; i < this->num_timestep; ++i)
+        for (int k = 0; k < this->num_features; ++k, ++ix)
+            output[ix] = float(input[i][k] - min_scaler[k]) / (max_scaler[k] - min_scaler[k]);
+}
+
+int CModelWrapper::preditct(const std::vector<std::vector<float>> input_data){
+    std::vector<float> scaled_input_data(this->num_timestep*this->num_features);
+    this->min_max_scaler(scaled_input_data, input_data);
+    auto output = this->model(cppflow::tensor(scaled_input_data,{1,this->num_timestep,this->num_features }));
     auto values = output.get_data<float>();
     float glob_max = -1;
     int res = 0;
