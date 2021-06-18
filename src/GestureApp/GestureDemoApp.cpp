1ab272562e0abcf67f364ee9d5cfe27ae31d22a2
tranvanh <tranvietanhcz@gmail.com>
2021-06-18 10:37:49 +0200

    implementation text

--- src/GestureApp/GestureDemoApp.cpp
+++ src/GestureApp/GestureDemoApp.cpp
@@ -10,7 +10,7 @@
 
 
 #define MODEL_DIR "./LSTM_model"
-#define TIMESTEP 90
+#define TIMESTEP 60
 #define NUM_FEATURES 31
 constexpr unsigned int hash(const char* s, int off = 0) {
 	return !s[off] ? 5381 : (hash(s, off + 1) * 33) ^ s[off];
